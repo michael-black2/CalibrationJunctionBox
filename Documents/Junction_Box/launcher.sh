d1a21c1b59fcc31fb1cfb3f85ccaecbecfa70420
Michael Black <michael.black@sensoscientific.com>
2022-05-31 14:53:33 -0700

    add launcher]

---
+++ Documents/Junction_Box/launcher.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+# launcher.sh
+
+cd /home/pi/Documents/Junction_Box
+PYTHONPATH=['/home/pi/.local/lib/python3.9/site-packages','home/pi/.local/lib/python3.9/site-packages/spidev-3.5-py3.9-linux-armv7l.egg']
+sudo python3 /home/pi/Documents/Junction_Box/JunctionBox.py
